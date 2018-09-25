5288a3183ff237d82e3ee103166f7f21e3e94f5c
Shi Hao Wang <txstudio@outlook.com>
2018-09-25 16:58:56 +0800

    加入事件紀錄資表物件

---
+++ eShop.Loader/Data/EventLogs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Loader
+{
+    public sealed class EventLog
+    {
+        public int No { get; set; }
+        public Nullable<DateTimeOffset> EventDateTime { get; set; }
+
+        public Nullable<Guid> MemberGUID { get; set; }
+        public string ProductSchema { get; set; }
+        public string ProductName { get; set; }
+
+        public short? OrginalStorage { get; set; }
+        public short? Quantity { get; set; }
+
+        public bool? IsSuccess { get; set; }
+        public string Exception { get; set; }
+    }
+}
